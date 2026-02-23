c88b2f7f6e59c063868377b5b1c77633d972f377
neto2000 <82058954+neto2000@users.noreply.github.com>
2026-02-23 11:09:54 +0100

    header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,4 +8,30 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<header class="head">
+	<p>test</p>
+	<button>Menu</button>
+</header>
+
+<style>
+
+.head {
+	
+	display: flex;
+	justify-content: space-between;
+	position: sticky;
+	top: 0;
+	padding: 10px 16px;
+	background: #555;
+	color: #f1f1f1;
+}
+
+:global(body) {
+
+	margin: 0;
+	padding: 0;
+}
+
+</style>
+
 {@render children()}
