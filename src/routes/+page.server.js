9b529f7c89c32c47d2e968ae43ec036fc7b4b90b
neto2000 <82058954+neto2000@users.noreply.github.com>
2026-03-03 19:33:28 +0100

    fixed undefined query error

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -4,20 +4,4 @@ import custom_pool from "../lib/server/db"
 export const load = async () => {
 
 	
-
-
-	try {
-
-		const [rows,fields] = await custom_pool.query('SELECT * from user')
-
-		console.log(rows)
-	} catch (err) {
-		console.log(err)
-	}	
-	
-
-
-	return {
-		test: "lol"
-	}
 }
