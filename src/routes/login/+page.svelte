a0d8ede60dfb4367eaa6cd7a2212a0f30dadc7f4
Weakz07 <dave@edv-berner.de>
2026-03-14 16:14:05 +0100

    Login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -48,15 +48,116 @@
 </script>
 
 
-<div>
+<div class="background">
+
+
+
+
 	
-	<form onsubmit={login}>	
-		<input type="text" bind:value={email} />
-		<input type="text" bind:value={password} />
+	<form class="container" onsubmit={login}>	
+		<div><p class="überschrift">Log in to FlatFinder</p></div>
+		<div class="text-container"><p class="texte">E-Mail</p></div>
+		<input class="eingabe" type="text" bind:value={email} />
+		<div class="text-container"><p class="texte">Passwort</p></div>
+		<input class="eingabe" type="text" bind:value={password} />
 	
-		<button type="submit">login</button>
+		<button class="bestätigen" type="submit">Log In</button>
+
+		<div><p class="registrieren">Noch nicht registriert? <a class="registerlink" href="/register">Registrieren</a></p></div>
+
+			<p style="color: red;">{error_msg}</p>
 	</form>
 
 
-	<p style="color: red;">{error_msg}</p>
+
+
+
 </div>
+
+<style>
+
+.background {
+
+background-color: blueviolet;
+height: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+
+}
+
+.container {
+
+background-color: white;
+width: auto;
+height: auto;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+padding: 50px;
+
+}
+
+.text-container {
+
+width: 308px;
+
+}
+
+.eingabe {
+
+width: 300px;
+height: 30px;
+margin-bottom: 15px;
+
+
+}
+
+.überschrift {
+
+font-size: 30px;
+width: 308px;
+height: auto;
+margin: 0px;
+margin-bottom: 30px;
+
+
+}
+
+.texte {
+
+margin: 0px;
+font-size: 15px;
+
+}
+
+.bestätigen {
+
+width: 308px;
+background-color: rgb(15, 15, 116);
+color: white;
+border: none;
+padding: 10px;
+font-size: 15px;
+
+
+}
+
+.registrieren {
+
+width: 308px;
+font-size: 15px;
+align-items: center;
+justify-content: center;
+
+}
+
+
+.registerlink {
+
+margin-left: 5px;
+
+}
+
+</style>
